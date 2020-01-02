bb511241bfb70e8d4db7127335c72290428db931
root <root@localhost.localdomain>
2020-01-02 14:51:09 +0800

    test

--- config/startup.sh
+++ config/startup.sh
@@ -7,5 +7,6 @@ PROJECT_NAME="elk-demo-1.0.0"
 JAVALOG_DIR="/opt/project/logs"
 DT=`date +"%Y%m%d_%H%M%S"`
 
+
 START_OPTS="$START_OPTS -Djava.io.tmpdir=${JAVALOG_DIR}"
 java $JMX_OPTS $START_OPTS -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=192.168.5.194 -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false -jar -server /opt/code/elk-demo/build/libs/${PROJECT_NAME}.jar
