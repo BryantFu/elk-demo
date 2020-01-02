51bc493f58282f4fe456e57c0e20c11232d1204e
chenfu <1062281158@qq.com>
2020-01-02 15:43:48 +0800

    添加启动参数

--- src/main/java/com/example/elkdemo/ElkDemoApplication.java
+++ src/main/java/com/example/elkdemo/ElkDemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.elkdemo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,19 +16,24 @@ import java.util.List;
 @RestController
 public class ElkDemoApplication {
 
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(ElkDemoApplication.class, args);
+
         log.info("============启动成功================");
     }
 
     @GetMapping(value = "/test")
     public String test() throws InterruptedException {
-        List<String> stringList = new ArrayList<>();
-        stringList.add("aa");
-        for (int i = 0; i < 10000; i++) {
-            Thread.sleep(500);
-            stringList.get(0);
-        }
+//        List<String> stringList = new ArrayList<>();
+//        stringList.add("aa");
+//        for (int i = 0; i < 10000; i++) {
+//            Thread.sleep(500);
+//            stringList.get(0);
+//        }
+        System.out.println(System.getProperty("APPID"));
         return "成功";
     }
 
